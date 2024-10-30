bb59ae5bc142956f1293387a2f8de58dac0b8284
mcraa <6892971+mcraa@users.noreply.github.com>
2024-10-30 11:23:49 +0100

    feat(init): add skeleton of generator and usage

---
+++ AutobinderGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Devies.Autobinder
+{
+
+    [Generator]
+    public class AutobinderGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            
+        }
+    }
+
+}
